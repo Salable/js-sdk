9a7705371921dd82af52d77396e686d9448a16d4
Joshua Okoro <jokoro@adaptavist.com>
2023-03-02 11:26:09 -0600

    chore: linting

--- cdn/bin/cdk.ts
+++ cdn/bin/cdk.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
-import { environment } from './constants';
+import {CdkStack} from '../lib/cdk-stack';
+import {environment} from './constants';
 
 const app = new cdk.App();
 
