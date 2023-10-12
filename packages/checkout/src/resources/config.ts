bd4edb1ba56d07666125ddfd5bfc666a7823ce06
Joshua Okoro <jokoro@adaptavist.com>
2023-10-12 11:32:42 -0500

    fix: using the right cdn url

--- packages/checkout/src/resources/config.ts
+++ packages/checkout/src/resources/config.ts
@@ -1,19 +1,21 @@
 const SALABLE_PUBLISHABLE_KEY = process.env.SALABLE_PUBLISHABLE_KEY?.trim();
 const SALABLE_LIVE_KEY = process.env.SALABLE_LIVE_KEY?.trim();
 const SALABLE_BASE_URL = process.env.SALABLE_BASE_URL?.trim();
-const SALABLE_BASE_CDN = process.env.SALABLE_BASE_URL?.trim();
+const CDN_DOMAIN = process.env.CDN_DOMAIN?.trim();
 const ENVIRONMENT = process.env.ENVIRONMENT?.trim();
 
 if (!SALABLE_PUBLISHABLE_KEY) throw new Error('Missing SALABLE_PUBLISHABLE_KEY');
 if (!SALABLE_LIVE_KEY) throw new Error('Missing SALABLE_LIVE_KEY');
 if (!SALABLE_BASE_URL) throw new Error('Missing SALABLE_BASE_URL');
-if (!SALABLE_BASE_CDN) throw new Error('Missing SALABLE_BASE_CDN');
+if (!CDN_DOMAIN) throw new Error('Missing CDN_DOMAIN');
 if (!ENVIRONMENT) throw new Error('Missing ENVIRONMENT');
+
+const CDNBase = !CDN_DOMAIN.startsWith('http') ? `https://${CDN_DOMAIN}` : CDN_DOMAIN;
 
 export const environment = {
   publishableKey: SALABLE_PUBLISHABLE_KEY,
   liveKey: SALABLE_LIVE_KEY,
   baseURL: SALABLE_BASE_URL,
-  baseCDN: SALABLE_BASE_CDN,
-  assert: ENVIRONMENT === 'prod' ? `${SALABLE_BASE_CDN}` : '../../../dist',
+  baseCDN: CDNBase,
+  assert: ENVIRONMENT === 'dev' ? '../../../dist' : `${CDNBase}/latest`,
 };
